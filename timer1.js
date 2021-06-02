88dc97872906b8621865e8dfa3e25e20a8f6b602
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-02 12:35:56 -0700

    refactored with a mentor so removed unnecessary codes

--- timer1.js
+++ timer1.js
@@ -1,13 +1,18 @@
 const args = process.argv.slice(2);
 
-const timer = () => {
-  for (const arg of args) {
+const timer = input => {
+  const checkLength = input.filter(element => !isNaN(element));
+  if (checkLength.length === 0) {
+    process.exit();
+  }
+
+  for (const arg of input) {
     setTimeout(() => {
-      if (!isNaN(arg) && arg >= 0) {
+      if (arg >= 0) {
         process.stdout.write('\x07');
         console.log(`${arg} second(s)`);
       }
     }, 1000 * arg);
   }
 };
-timer();
+timer(args);
